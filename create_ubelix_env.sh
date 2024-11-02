d33d0cf79482af8193e4b1bc48d861d5153a3b67
Zaharia, Tudorita (STUDENTS) <tudorita.zaharia@students.unibe.ch>
2024-11-02 13:51:52 +0100

    bash

--- create_ubelix_env.sh
+++ create_ubelix_env.sh
@@ -5,10 +5,10 @@ module load Anaconda3
 eval "$(conda shell.bash hook)"
 
 # Create a new environment for MIALab
-conda create --name mialab python=3.8
+conda create --name mialab python=3.11.0
 
 # Activate the new environment
 conda activate mialab
 
 # Install the MIALab dependencies
-pip install -r ../requirements.txt
+pip install -r requirements.txt
