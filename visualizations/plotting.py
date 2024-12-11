35b755c697f5447ca44d00b26d9b3b5531fcb2b4
Zaharia, Tudorita (STUDENTS) <tudorita.zaharia@students.unibe.ch>
2024-12-11 19:04:22 +0100

    mia-results

--- visualizations/plotting.py
+++ visualizations/plotting.py
@@ -3,6 +3,7 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from datetime import datetime
+import numpy as np
 import os
 
 def save_plot_with_timestamp(plt, title, output_path):
@@ -62,5 +63,40 @@ def boxplot_labels(METRICS,LABELS,data,TITLE,output_path):
         plt.tight_layout()
         save_plot_with_timestamp(plt,f"{metric}_{TITLE}",output_path)
 
-def boxplot_experiment_comparison(data1,data2,title1,title2,output_path):
-    pass 
+def boxplot_experiment_comparison(data,metric,x_titles,TITLE,output_path):
+
+    overall_median = np.median(data)
+
+    fig = plt.figure(figsize=(10,6))
+    ax = fig.add_subplot(111)
+
+    bp = ax.boxplot(data,patch_artist=True,widths=0.6)
+    ax.axhline(y=overall_median, color='red', linestyle='--', linewidth=2, label=f"Median: {overall_median:.2f}")
+
+
+    plt.setp(bp['boxes'], color='grey')
+    plt.setp(bp['whiskers'], color='000')
+    plt.setp(bp['caps'], color='000')
+    plt.setp(bp['caps'], linewidth=0)
+    plt.setp(bp['medians'], color='000')
+    plt.setp(bp['medians'], linewidth=1.5)
+    plt.setp(bp['fliers'], marker='.')
+    plt.setp(bp['fliers'], markerfacecolor='black')
+    plt.setp(bp['fliers'], alpha=1)
+
+    ax.set_title(TITLE, fontweight='bold', fontsize=12)
+    ax.set_ylabel(metric, fontweight='bold', fontsize=12)
+    ax.yaxis.set_tick_params(labelsize=12)
+
+    ax.set_xticklabels(x_titles, fontdict={'fontsize': 12, 'fontweight': 'bold'})
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_linewidth(2)
+    ax.spines['bottom'].set_linewidth(2)
+    ax.set_title(f"{metric}:{TITLE}",fontdict={'fontweight': 'bold','fontsize': 14})
+
+    ax.set_ylim(0.0, 1.0)
+    
+    # Display the plot
+    plt.tight_layout()
+    save_plot_with_timestamp(plt,f"{metric}_{TITLE}",output_path)
